cfdb7bb80141cd6bb4a64d1fd31b0571ccd40231
marwanEsm <marwan.esmaail85@gmail.com>
2024-09-08 18:12:13 +0200

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
-import logo from './logo.svg';
-const App = () => {
+
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
