9f679ce483fea2c31ec68af5b4a88bb727a520c8
marwanEsm <marwan.esmaail85@gmail.com>
2024-10-27 09:39:14 +0100

    modify login file

--- src/components/elements/Footer/Footer.tsx
+++ src/components/elements/Footer/Footer.tsx
@@ -4,24 +4,30 @@ import { FaFacebookF, FaInstagram, FaRss } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <footer className="bg-[#8241B4] text-white py-4 px-6 flex justify-between items-center w-full">
-            <div className="text-sm">
-                © Über den Tellerrand {new Date().getFullYear()}
-            </div>
-            <div className="flex space-x-4">
-                <Link to="#" aria-label="Facebook">
-                    <FaFacebookF className="w-5 h-5" />
-                </Link>
-                <Link to="#" aria-label="Instagram">
-                    <FaInstagram className="w-5 h-5" />
-                </Link>
-                <Link to="#" aria-label="RSS Feed">
-                    <FaRss className="w-5 h-5" />
-                </Link>
-            </div>
-        </footer>
-    );
-}
+  return (
+    <footer className="bg-[#8241B4] text-white py-4 px-6 flex justify-between items-center w-full">
+      <div className="text-sm">
+        © Über den Tellerrand {new Date().getFullYear()}
+      </div>
+      <div className="flex space-x-4">
+        <Link
+          to="https://www.facebook.com/ueberdentellerrandkochen/"
+          aria-label="Facebook"
+        >
+          <FaFacebookF className="w-5 h-5" />
+        </Link>
+        <Link
+          to="https://www.instagram.com/ueber_den_tellerrand/"
+          aria-label="Instagram"
+        >
+          <FaInstagram className="w-5 h-5" />
+        </Link>
+        <Link to="#" aria-label="RSS Feed">
+          <FaRss className="w-5 h-5" />
+        </Link>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,54 +1,51 @@
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Cooking from "../assets/Cooking.png";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Community from "../assets/Community.png";
 import Together from "../assets/Together.png";
 import Women from "../assets/Women.png";
 import LoginForm from "../components/overlay/login/Login";
-
+// const imageClassName = "w-full h-[calc(50vh)]"
 const Home = () => {
-    return (
-        <div className="flex flex-col w-full h-screen">
-            <div className="flex-grow h-1/2">
-                <Carousel
-                    autoPlay
-                    infiniteLoop
-                    interval={2000}
-                    showThumbs={false}
-                    showStatus={false}
-                    className="w-full h-full"
-                >
-                    <div className="w-full h-full">
-                        <img
-                            src={Cooking}
-                            alt="Gemeinsames Kochen"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    <div className="w-full h-full">
-                        <img
-                            src={Women}
-                            alt="Leckere Gerichte"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    <div className="w-full h-full">
-                        <img
-                            src={Together}
-                            alt="Gemeinschaftstreffen"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                </Carousel>
-            </div>
+  return (
+    <div className="flex flex-col w-full h-screen overflow-hidden">
+      <div className="flex-grow">
+        <Carousel
+          autoPlay
+          infiniteLoop
+          interval={2000}
+          showThumbs={false}
+          showStatus={false}
+          className="w-full h-[100px]"
+        >
+          <img
+            src={Women}
+            alt="Leckere Gerichte"
+            className="w-full h-[50vh]object-cover"
+          />
 
-            <div className="flex-grow h-1/2 flex justify-center items-center">
-                <div>
-                    <h1 className="text-center text-3xl font-bold mb-4 text-purple-600">Willkommen zurück!</h1>
-                    <LoginForm />
-                </div>
-            </div>
+          <img
+            src={Together}
+            alt="Gemeinschaftstreffen"
+            className="w-full h-[50vh]object-cover"
+          />
+          <img
+            src={Community}
+            alt="Gemeinschaftstreffen"
+            className="w-full h-[50vh]object-cover"
+          />
+        </Carousel>
+      </div>
+
+      <div className="flex-grow flex justify-center items-center">
+        <div>
+          <h1 className="text-center text-3xl font-bold mb-4 text-purple-600">
+            Willkommen zurück!
+          </h1>
+          <LoginForm />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
