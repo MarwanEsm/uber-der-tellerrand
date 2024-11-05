a3db422a61561ea42fa0277e4cd6904ad6dc2747
marwanEsm <marwan.esmaail85@gmail.com>
2024-11-05 10:26:00 +0100

    add form

--- src/screens/EventForm.tsx
+++ src/screens/EventForm.tsx
@@ -1,12 +1,12 @@
-// src/components/EventForm.tsx
-
 import { getAuth } from "firebase/auth";
-import { doc, updateDoc } from "firebase/firestore"; // Firestore functions
+import { doc, updateDoc } from "firebase/firestore";
 import React, { FormEvent, useState } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useNavigate } from "react-router-dom";
 import { db } from "../firebase";
 
-const EventForm: React.FC = () => {
+const EventFormCarousel: React.FC = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -15,8 +15,9 @@ const EventForm: React.FC = () => {
     leadRecipe: "",
     discoveryMethod: ""
   });
-  const [submitMessage, setSubmitMessage] = useState<string | null>(null); // Message for submission status
+  const [submitMessage, setSubmitMessage] = useState<string | null>(null);
   const navigate = useNavigate();
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
@@ -36,11 +37,9 @@ const EventForm: React.FC = () => {
       const user = auth.currentUser;
 
       if (user) {
-        // Update firstLogin flag to false in Firestore
         const userDocRef = doc(db, "users", user.uid);
         await updateDoc(userDocRef, { firstLogin: false });
 
-        // Optional: Store additional form data (e.g., name, allergyInfo)
         await updateDoc(userDocRef, {
           name: formData.name,
           email: formData.email,
@@ -50,8 +49,8 @@ const EventForm: React.FC = () => {
           discoveryMethod: formData.discoveryMethod
         });
 
-        setSubmitMessage("Anmeldeformular erfolgreich abgeschickt!"); // Show success message
-        setTimeout(() => navigate("/events"), 3000); // Redirect to /events after 3 seconds
+        setSubmitMessage("Anmeldeformular erfolgreich abgeschickt!");
+        setTimeout(() => navigate("/events"), 3000);
       }
     } catch (error) {
       console.error("Error updating document:", error);
@@ -61,40 +60,105 @@ const EventForm: React.FC = () => {
     }
   };
 
+  const nextSlide = () => setCurrentSlide((prev) => Math.min(prev + 1, 4));
+  const prevSlide = () => setCurrentSlide((prev) => Math.max(prev - 1, 0));
+
   return (
-    <div className="ml-[10px] mr-[10px] max-w-lg p-6 bg-gradient-to-r from-purple-300 via-purple-400 to-purple-600 rounded-lg shadow-lg text-white">
-      <h2 className="text-center text-2xl mb-6">Anmeldeformular</h2>
-      {submitMessage && (
-        <div className="mb-4 text-center text-sm p-2 bg-green-500 rounded">
-          {submitMessage}
+    <div className="max-w-lg w-full mx-auto p-6 bg-white rounded-lg shadow-lg text-gray-800">
+      <Carousel
+        selectedItem={currentSlide}
+        showThumbs={false}
+        showStatus={false}
+        showArrows={false}
+        swipeable={false}
+        emulateTouch={false}
+        onChange={setCurrentSlide}
+      >
+        {/* Slide 1 */}
+        <div className="p-6 text-center">
+          <h2 className="text-3xl font-bold text-purple-700 mb-2">
+            IT IS SOMMER TIME!!! 08.06.2024 um 16 Uhr
+          </h2>
+          <p className="text-center text-gray-700 mb-6">
+            Liebe*r Teilnehmende,
+            <br />
+            <br />
+            wie schön, dass du mit <strong>DABEI SEIN</strong> möchtest! Um Dir
+            einen Platz zu sichern, füll bitte dieses Formular aus. Daraufhin
+            wirst Du von uns eine Nachricht mit allen weiteren Infos bekommen.
+            <br />
+            <br />
+            Wir <strong>freuen uns auf Dich!</strong>
+            <br />
+            Dein Über den Tellerrand-Team Osnabrück
+          </p>
         </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
+
+        {/* Slide 2 */}
+        <div className="p-6">
+          <p className="text-gray-700 mb-4">
+            Um den Kontakt zu erleichtern und unsere Aktivitäten besser zu
+            organisieren, benötigen wir deine persönlichen Informationen sowie
+            deine E-Mail-Adresse.
+            <br />
+            <br />
+            Bitte markiere das Kästchen unten, um uns die Erlaubnis zur Nutzung
+            deiner Daten zu erteilen.
+            <br />
+            <br />
+            Deine Informationen werden sicher online in einem geschützten Ordner
+            auf Google Drive gespeichert. Du hast jederzeit die Möglichkeit,
+            deine Zustimmung zur Nutzung deiner Informationen zu widerrufen oder
+            uns per E-Mail zu kontaktieren, um Änderungen oder Löschungen deiner
+            Daten zu beantragen.
+          </p>
+          <label className="flex items-center space-x-2 mt-4">
+            <input
+              type="checkbox"
+              name="consent"
+              onChange={handleChange}
+              className="form-checkbox h-5 w-5 text-orange-500"
+              required
+            />
+            <span>Ja, ich erlaube die Nutzung meiner Daten</span>
+          </label>
+        </div>
+
+        {/* Slide 3 - Name and Email */}
+        <div className="p-6">
+          <h3 className="text-xl font-semibold mb-4">Anmeldeformular</h3>
+          <p className="text-gray-700 mb-6">
+            Die Schritte zur Registrierung:
+            <br />
+            1. Ausfüllen des Anmeldeformulars.
+            <br />
+            2. Warten auf die Bestätigungs-E-Mail (innerhalb von 7 Tagen nach
+            Absendung des Formulars)
+          </p>
           <label className="block mb-2">Name *</label>
           <input
             type="text"
             name="name"
             value={formData.name}
             onChange={handleChange}
-            className="w-full px-4 py-2 bg-transparent border-b border-gray-300 text-white focus:outline-none placeholder-white"
+            className="w-full px-4 py-2 mb-4 border rounded focus:outline-none"
             placeholder="Dein Name"
             required
           />
-        </div>
-        <div className="mb-4">
           <label className="block mb-2">E-Mail *</label>
           <input
             type="email"
             name="email"
             value={formData.email}
             onChange={handleChange}
-            className="w-full px-4 py-2 bg-transparent border-b border-gray-300 text-white focus:outline-none placeholder-white"
+            className="w-full px-4 py-2 mb-4 border rounded focus:outline-none"
             placeholder="Deine E-Mail-Adresse"
             required
           />
         </div>
-        <div className="mb-4">
+
+        {/* Slide 4 - Allergy & Instrument */}
+        <div className="p-6">
           <label className="block mb-2">
             Hast Du eine Allergie gegen Lebensmittel?
           </label>
@@ -103,11 +167,9 @@ const EventForm: React.FC = () => {
             name="allergyInfo"
             value={formData.allergyInfo}
             onChange={handleChange}
-            className="w-full px-4 py-2 bg-transparent border-b border-gray-300 text-white focus:outline-none placeholder-white"
+            className="w-full px-4 py-2 mb-4 border rounded focus:outline-none"
             placeholder="Schreibe hier deine Allergie"
           />
-        </div>
-        <div className="mb-4">
           <label className="block mb-2">
             Welches Instrument bringst Du mit?
           </label>
@@ -116,11 +178,13 @@ const EventForm: React.FC = () => {
             name="instrument"
             value={formData.instrument}
             onChange={handleChange}
-            className="w-full px-4 py-2 bg-transparent border-b border-gray-300 text-white focus:outline-none placeholder-white"
+            className="w-full px-4 py-2 border rounded focus:outline-none"
             placeholder="Instrument"
           />
         </div>
-        <div className="mb-4">
+
+        {/* Slide 5 - Recipe & Discovery */}
+        <div className="p-6">
           <label className="block mb-2">
             Möchtest Du ein Rezept einbringen und eine Kochstation leiten?
           </label>
@@ -129,17 +193,17 @@ const EventForm: React.FC = () => {
             name="leadRecipe"
             value={formData.leadRecipe}
             onChange={handleChange}
-            className="w-full px-4 py-2 bg-transparent border-b border-gray-300 text-white focus:outline-none placeholder-white"
+            className="w-full px-4 py-2 mb-4 border rounded focus:outline-none"
             placeholder="Ja oder Nein"
           />
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2">Wie hast Du von uns erfahren?</label>
+          <label className="block mb-2">
+            Wie hast Du von Über den Tellerrand Osnabrück erfahren?
+          </label>
           <select
             name="discoveryMethod"
             value={formData.discoveryMethod}
             onChange={handleChange}
-            className="w-full px-4 py-2 bg-transparent border-b border-gray-300 text-white focus:outline-none placeholder-white"
+            className="w-full px-4 py-2 border rounded focus:outline-none"
           >
             <option value="">Bitte auswählen</option>
             <option value="Flyer">Über einen Flyer</option>
@@ -149,15 +213,45 @@ const EventForm: React.FC = () => {
             <option value="Other">Sonstiges</option>
           </select>
         </div>
-        <button
-          type="submit"
-          className="block w-full bg-orange-500 text-white py-2 px-4 rounded-full hover:bg-orange-600 transition shadow-lg transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-orange-300"
-        >
-          Formular absenden
-        </button>
-      </form>
+
+        {/* Last Slide - Confirmation */}
+        <div className="p-6 text-center">
+          <h3 className="text-2xl font-semibold mb-4">
+            Bestätigen Sie Ihre Anmeldung
+          </h3>
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="w-full bg-orange-500 text-white py-2 px-4 rounded-full hover:bg-orange-600 transition focus:outline-none focus:ring-4 focus:ring-orange-300"
+          >
+            Formular absenden
+          </button>
+          {submitMessage && (
+            <div className="mt-4 text-center text-sm p-2 bg-green-500 rounded">
+              {submitMessage}
+            </div>
+          )}
+        </div>
+      </Carousel>
+
+      {/* Custom Navigation Buttons */}
+      <div className="flex justify-between mt-4">
+        {currentSlide > 0 && (
+          <button onClick={prevSlide} className="px-4 py-2 bg-gray-300 rounded">
+            Back
+          </button>
+        )}
+        {currentSlide < 4 && (
+          <button
+            onClick={nextSlide}
+            className="px-4 py-2 bg-gray-300 rounded ml-auto"
+          >
+            Next
+          </button>
+        )}
+      </div>
     </div>
   );
 };
 
-export default EventForm;
+export default EventFormCarousel;
