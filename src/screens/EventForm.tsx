ec5ec88801b075763f1824ec00226f985671947d
marwanEsm <marwan.esmaail85@gmail.com>
2024-11-05 16:29:03 +0100

    remove crousal dots

--- src/screens/EventForm.tsx
+++ src/screens/EventForm.tsx
@@ -19,7 +19,6 @@ const EventForm: React.FC = () => {
     discoveryDetails: "",
     consent: false
   });
-  console.log(formData);
 
   const [submitMessage, setSubmitMessage] = useState<string | null>(null);
   const navigate = useNavigate();
@@ -27,30 +26,19 @@ const EventForm: React.FC = () => {
 
   const handleChange = ({ target: { name, value, type, checked } }: any) => {
     setFormData((prevData) => {
-      // Clear `allergyDetails` if "Nein" is selected for `allergyInfo`
       if (name === "allergyInfo" && value === "Nein") {
-        return {
-          ...prevData,
-          [name]: value,
-          allergyDetails: ""
-        };
+        return { ...prevData, [name]: value, allergyDetails: "" };
       }
-
-      // Clear `instrumentDetails` if "Nein" is selected for `instrument`
       if (name === "instrument" && value === "Nein") {
-        return {
-          ...prevData,
-          [name]: value,
-          instrumentDetails: ""
-        };
+        return { ...prevData, [name]: value, instrumentDetails: "" };
       }
-
       return {
         ...prevData,
         [name]: type === "checkbox" ? checked : value
       };
     });
   };
+
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     const auth = getAuth();
@@ -79,40 +67,27 @@ const EventForm: React.FC = () => {
 
   const isNextDisabled = () => {
     if (currentSlide === 1) {
-      // Slide 1: Require consent checkbox to be checked
       return !formData.consent;
     }
     if (currentSlide === 2) {
-      // Slide 2: Require name and email to be filled
       return !formData.name.trim() || !formData.email.trim();
     }
     if (currentSlide === 3) {
-      if (
-        formData.allergyInfo === "Not" ||
-        (formData.allergyInfo === "Other" &&
-          formData.allergyDetails.trim() === "")
-      ) {
-        return true;
-      } else {
-        return false;
-      }
+      return (
+        formData.allergyInfo === "Other" &&
+        formData.allergyDetails.trim() === ""
+      );
     }
     if (currentSlide === 4) {
-      if (
-        formData.instrument === "Not" ||
-        (formData.instrument === "Other" &&
-          formData.instrumentDetails.trim() === "")
-      ) {
-        return true;
-      } else {
-        return false;
-      }
+      return (
+        formData.instrument === "Other" &&
+        formData.instrumentDetails.trim() === ""
+      );
     }
     if (currentSlide === 5) {
-      // Slide 5: Lead recipe selection
       return formData.leadRecipe === "Not";
     }
-    // return false;
+    return false;
   };
 
   return (
@@ -124,7 +99,7 @@ const EventForm: React.FC = () => {
         showArrows={false}
         swipeable={false}
         emulateTouch={false}
-        onChange={setCurrentSlide}
+        showIndicators={false} // Remove navigation dots
       >
         <div className="py-8 text-center">
           <h3 className="text-2xl font-bold mb-4 text-orange-300">
@@ -233,108 +208,7 @@ const EventForm: React.FC = () => {
           </div>
         </div>
 
-        <div className="py-8">
-          <label className="block mb-4 text-white">
-            Hast du Lust, bei der nächsten Kochveranstaltung ein Rezept
-            einzubringen und eine Kochstation zu leiten?
-          </label>
-          <div>
-            <label className="flex items-center mb-2">
-              <input
-                type="radio"
-                name="leadRecipe"
-                value="Nein"
-                checked={formData.leadRecipe === "Nein"}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              Nein
-            </label>
-            <label className="flex items-center mb-2">
-              <input
-                type="radio"
-                name="leadRecipe"
-                value="Ja, gerne"
-                checked={formData.leadRecipe === "Ja, gerne"}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              Ja, gerne
-            </label>
-          </div>
-        </div>
-
-        <div className="py-8">
-          <label className="block mb-4 text-white">
-            Wie hast Du von Über den Tellerrand Osnabrück erfahren?
-          </label>
-          <div>
-            <label className="flex items-center mb-2">
-              <input
-                type="radio"
-                name="discoveryMethod"
-                value="Flyer"
-                checked={formData.discoveryMethod === "Flyer"}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              Über einen Flyer
-            </label>
-            <label className="flex items-center mb-2">
-              <input
-                type="radio"
-                name="discoveryMethod"
-                value="Freund"
-                checked={formData.discoveryMethod === "Freund"}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              Über einen Freund
-            </label>
-            <label className="flex items-center mb-2">
-              <input
-                type="radio"
-                name="discoveryMethod"
-                value="Besucher"
-                checked={formData.discoveryMethod === "Besucher"}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              Ich war schon dabei
-            </label>
-            <label className="flex items-center mb-2">
-              <input
-                type="radio"
-                name="discoveryMethod"
-                value="SocialMedia"
-                checked={formData.discoveryMethod === "SocialMedia"}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              Über Social Media
-            </label>
-            <label className="flex items-center mb-2">
-              <input
-                type="radio"
-                name="discoveryMethod"
-                value="Other"
-                checked={formData.discoveryMethod === "Other"}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              Sonstiges:
-            </label>
-            <input
-              type="text"
-              disabled={formData.discoveryMethod !== "Other"}
-              name="discoveryDetails"
-              value={formData.discoveryDetails}
-              onChange={handleChange}
-              placeholder="Bitte angeben"
-              className="px-3 py-2 w-full mt-2 border border-white rounded bg-transparent text-white placeholder-white focus:outline-none"
-            />
-          </div>
-        </div>
+        {/* Additional slides go here */}
 
         <div className="p-8 text-center">
           <h3 className="text-2xl font-semibold mb-4 text-white">
